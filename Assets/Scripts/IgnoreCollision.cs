5ffcf87e0f44962f396fbe8eac552d57e9723365
Jocoba <jserthan@gmail.com>
2021-10-21 17:00:25 -0400

    added joints

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(gameObject.layer, gameObject.layer);
+    }
+
+}
